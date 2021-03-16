7eea4c09dbfe03ffd96682a7da231f36cd5c0bce
Rasmus Syberg <rvs@brickshare.io>
2021-03-16 10:42:11 +0100

    fix: It should not return with parenthesis
    It is a bit confusing since it's not a direct standin instead of ? for prepared statements.

--- test/unit/tag.test.ts
+++ test/unit/tag.test.ts
@@ -10,7 +10,7 @@ describe('sql template', () => {
   });
 
   it(`should properly handle array input`, () => {
-    expect({ ...SQL`SELECT * FROM table WHERE id IN ${[1, 2, 3, 4]} AND name = ${'hello'}` }).toEqual({
+    expect({ ...SQL`SELECT * FROM table WHERE id IN (${[1, 2, 3, 4]}) AND name = ${'hello'}` }).toEqual({
       statement: `SELECT * FROM table WHERE id IN (?,?,?,?) AND name = ?`,
       arguments: ['1', '2', '3', '4', 'hello']
     });
@@ -18,7 +18,7 @@ describe('sql template', () => {
 
   it(`should handle concatenation of statements`, () => {
     const ids = [1, 2, 3, 4];
-    const statement = SQL`SELECT * FROM (${SQL`SELECT * FROM table WHERE id IN ${ids} AND name = ${'hello'}`})`;
+    const statement = SQL`SELECT * FROM (${SQL`SELECT * FROM table WHERE id IN (${ids}) AND name = ${'hello'}`})`;
     expect({ ...statement }).toEqual({
       statement: `SELECT * FROM (SELECT * FROM table WHERE id IN (?,?,?,?) AND name = ?)`,
       arguments: ['1', '2', '3', '4', 'hello']
